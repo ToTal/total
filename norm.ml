6548b0e69e9eac7939fc06b0019405860c69d4d9
Francisco Ferreira <fferre8@cs.mcgill.ca>
2014-06-06 19:37:43 -0400

    reimplementing iota reductions (still ugly)

--- norm.ml
+++ norm.ml
@@ -9,30 +9,61 @@ let rec head_is_elim sigma = function
   | Const x, l -> is_elim sigma x
   | _ -> false
 
+let empty = function | [] -> true | _ -> false
 
 (* The new normal *)
-let new_norm ?(weak=false) =
+let norm ?(weak=false) =
   let rec norm (sigma, gamma as ctx) ((e', loc) as e) =
     let h_e, sp_e = split_head_spine ctx e in
+    let empty_sp = empty sp_e in
     match h_e with
+    | Lambda a, loc when empty_sp ->
+       Lambda (norm_abstraction ctx a), loc
+    | Lambda (_, _, body), loc -> (* lambda with a spine triggers a beta reduction *)
+       norm ctx (join_head_spine (norm ctx (mk_subst (Dot (List.hd sp_e, idsubst)) body)) (List.tl sp_e))
+    | Const x, loc when is_elim sigma x && not empty_sp -> (* may trigger an iota reduction *)
+       iota ctx x sp_e
     | Const x, loc -> 
        begin match lookup_definition x sigma with
 	     | None -> e
 	     | Some e -> norm ctx e
        end
+    | Subst (s, e), loc -> 
+       norm ctx (join_head_spine (subst s e) sp_e)
     | Var _, loc 
     | Free _, loc -> 
        let sp_e = if weak then sp_e else List.map (norm ctx) sp_e in
        join_head_spine h_e sp_e
-		    
-    | _ -> assert false
+    | Universe _, loc when empty_sp ->  e
+    | Universe _, loc -> Error.violation "Unexpected non-empty spine with a Universe head."
+    | Pi a, loc when empty_sp -> Pi (norm_abstraction ctx a), loc
+    | Pi a, loc -> Error.violation "Unexpected non-empty spine with a Pi head."
+    | App _, _ -> Error.violation "Unexpected, head cannot be an application."
+    | Ann (e, t), loc when empty_sp -> Ann(norm ctx e, norm ctx t), loc
+    | Ann (e, t), loc -> norm ctx (join_head_spine e sp_e) (* removes annotation *)
 
+  and norm_abstraction (sigma, gamma as ctx) ((x, t, e) as a) =
+    if weak then a
+    else (x, norm ctx t, norm (sigma, extend gamma (x, t)) e)
+
+  and iota (sigma, gamma as _ctx) const sp =
+    let sp_len = List.length sp in
+    let t, n, d = Util.this (lookup_elim const sigma) in
+    if n == sp_len then
+      let x = List.nth sp 0 in
+      
+      (Print.warning "Unimplemented iota reduction" ;
+       join_head_spine (mk_const const) sp)
+    else
+      (Print.debug "ι reduction is stuck (more parameters required)" ;
+       join_head_spine (mk_const const) sp) (* TODO some location information is lost here *)
   in
   norm
 
+
 (** [norm env e] evaluates expression [e] in environment [env] to a weak head normal form,
     while [norm weak:false env e] evaluates to normal form. *)
-let norm ?(weak=false) =
+let norm_old ?(weak=false) =
   let rec norm (sigma, gamma as ctx) ((e', loc) as e) =
     match e' with
       | Var k -> e
@@ -56,10 +87,10 @@ let norm ?(weak=false) =
 	    (* Iota - reduction *)
 	    | App _ when head_is_elim sigma e1 -> 
 	       let (Const x, _), _ = split_head_spine ctx e1 in
-	       let Some (delim, _, d) = lookup_elim x sigma in
+	       let delim, _, d = Util.this (lookup_elim x sigma) in
 	       iota ctx (App (e1, e2), loc) delim d
 	    | Const x when is_elim sigma x -> 
-	       let Some (delim, _, d) = lookup_elim x sigma in
+	       let delim, _, d = Util.this(lookup_elim x sigma) in
 	       iota ctx (App (e1, e2), loc) delim d
 
             | Var _ | Free _ | Const _ | App _ -> 
@@ -77,68 +108,69 @@ let norm ?(weak=false) =
     then a
     else (x, norm ctx t, norm (sigma, extend gamma (x, t)) e)
   
-  and iota (sigma, gamma as ctx) e delim d = e
-    (* Print.debug "ι-reduction of : %t" (Print.expr ctx e) ; *)
-    (* let rec recs p mvec =  *)
-    (*   Print.debug "Called recs"; *)
-    (*   function *)
-    (*   | [] -> [] *)
-    (*   | (e, t)::es when is_constr d t -> *)
-    (* 	 Print.debug "Recs: recursive %t" (Print.expr ctx e) ; *)
-    (* 	 let Some elim = lookup_elim_for_ty d sigma in *)
-    (* 	 (join_head_spine (mk_const elim) (e::p::mvec)):: recs p mvec es *)
-    (*   | _::es -> recs p mvec es *)
-    (* in *)
-    (* let h, sp = split_head_spine e in *)
-    (* Print.debug "sp = [%t]" (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e)  sp  ) ; *)
-    (* let x = (match norm ctx h with *)
-    (* | Const x, _ -> *)
-    (*    (if (is_elim sigma x) then x *)
-    (* 	else Error.violation "iota reduction called in something that is not an eliminator") *)
-    (* | _ -> Error.violation "iota reudction does not contain an eliminator in its head") *)
-    (* in *)
-    (* let t, n = match (lookup_elim x sigma) with *)
-    (*   |	Some (t, n, _) -> t, n *)
-    (*   | None -> Error.violation "this cannot happen" in *)
-    (* if n = List.length sp then *)
-    (*   (\* the head of the spine contains the target *\) *)
-    (*   (Print.debug "Target: %t" (Print.expr ctx (List.hd (List.rev sp))) ; *)
-    (*   let t_hd, t_sp = split_head_spine (norm ctx (List.hd (List.rev sp))) in *)
-    (*   Print.debug "Target head: %t" (Print.expr ctx t_hd) ; *)
-    (*   Print.debug "Target spine: [%t]" (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e) t_sp); *)
-    (*   (match t_hd with *)
-    (*    (\* the normal form of the target is constructor *\) *)
-    (*    | Const x, l -> *)
-    (* 	  let t_sp_types = let tel,_ = get_telescope (lookup_ty x sigma) in List.map snd tel in *)
-    (* 	  let p, mvec = *)
-    (* 	    (match List.rev sp  with *)
-    (* 	     | _::p:: mvec -> p, mvec | _ -> Error.violation "The spine is too short!") *)
-    (* 	  in *)
-    (* 	  Print.debug "The motive P = %t" (Print.expr ctx p) ; *)
-    (* 	  Print.debug "mvec = %t" (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e) mvec) ; *)
-    (* 	  let i = (match lookup_constr_number x sigma with *)
-    (* 		   | Some i -> i | _ -> Error.violation "Constructor has to exist") *)
-    (* 	  in *)
-    (* 	  let mi = List.nth mvec i in *)
-    (* 	  Print.debug "The %d th method is: %t" i (Print.expr ctx mi) ; *)
-    (* 	  let rs = recs p mvec (Util.zip t_sp t_sp_types) in *)
-    (* 	  Print.debug "The recursive calls are: [%t]"  *)
-    (* 		      (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e) rs) ; *)
-    (* 	  let res = join_head_spine mi (t_sp @ rs) in *)
-    (* 	  Print.debug "@[%t@]@ reduces to @[%t@]" (Print.expr ctx e) (Print.expr ctx res) ; *)
-    (* 	  res *)
+  and iota (sigma, gamma as ctx) e delim d = 
+    Print.debug "ι-reduction of : %t" (Print.expr ctx e) ;
+    let rec recs p mvec =
+      Print.debug "Called recs";
+      function
+      | [] -> []
+      | (e, t)::es when is_constr ctx d t ->
+    	 Print.debug "Recs: recursive %t" (Print.expr ctx e) ;
+    	 let elim = Util.this (lookup_elim_for_ty d sigma) in
+    	 (join_head_spine (mk_const elim) (e::p::mvec)):: recs p mvec es
+      | _::es -> recs p mvec es
+    in
+    let h, sp = split_head_spine ctx e in
+    Print.debug "sp = [%t]" (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e)  sp  ) ;
+    let x = (match norm ctx h with
+    | Const x, _ ->
+       (if (is_elim sigma x) then x
+    	else Error.violation "iota reduction called in something that is not an eliminator")
+    | _ -> Error.violation "iota reudction does not contain an eliminator in its head")
+    in
+    let t, n = match (lookup_elim x sigma) with
+      |	Some (t, n, _) -> t, n
+      | None -> Error.violation "this cannot happen" in
+    if n = List.length sp then
+      (* the head of the spine contains the target *)
+      (Print.debug "Target: %t" (Print.expr ctx (List.hd (List.rev sp))) ;
+      let t_hd, t_sp = split_head_spine ctx (norm ctx (List.hd (List.rev sp))) in
+      Print.debug "Target head: %t" (Print.expr ctx t_hd) ;
+      Print.debug "Target spine: [%t]" (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e) t_sp);
+      (match t_hd with
+       (* the normal form of the target is constructor *)
+       | Const x, l ->
+    	  let t_sp_types = let tel,_ = get_telescope ctx (lookup_ty x sigma) in List.map snd tel in
+    	  let p, mvec =
+    	    (match List.rev sp  with
+    	     | _::p:: mvec -> p, mvec | _ -> Error.violation "The spine is too short!")
+    	  in
+    	  Print.debug "The motive P = %t" (Print.expr ctx p) ;
+    	  Print.debug "mvec = %t" (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e) mvec) ;
+    	  let i = (match lookup_constr_number x sigma with
+    		   | Some i -> i | _ -> Error.violation "Constructor has to exist")
+    	  in
+    	  let mi = List.nth mvec i in
+    	  Print.debug "The %d th method is: %t" i (Print.expr ctx mi) ;
+    	  let rs = recs p mvec (Util.zip t_sp t_sp_types) in
+    	  Print.debug "The recursive calls are: [%t]"
+    		      (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e) rs) ;
+    	  let res = join_head_spine mi (t_sp @ rs) in
+    	  Print.debug "@[%t@]@ reduces to @[%t@]" (Print.expr ctx e) (Print.expr ctx res) ;
+    	  res
 	  
-    (*    (\* the eliminator is stuck *\) *)
-    (*    | _ -> *)
-    (* 	  Print.debug *)
-    (* 	    "Stuck eliminator: @[ %t @]@ with head: @[ %t @]" *)
-    (* 	    (Print.expr ctx e) (Print.expr ctx t_hd); *)
-    (* 	  e)) *)
-    (* else *)
-    (*   (Print.debug "n= %d | len(sp) = %d[[%t]]" n (List.length sp) (Print.expr ctx e) ; *)
-    (*   e) *)
+       (* the eliminator is stuck *)
+       | _ ->
+    	  Print.debug
+    	    "Stuck eliminator: @[ %t @]@ with head: @[ %t @]"
+    	    (Print.expr ctx e) (Print.expr ctx t_hd);
+    	  e))
+    else
+      (Print.debug "n= %d | len(sp) = %d[[%t]]" n (List.length sp) (Print.expr ctx e) ;
+      e)
   in
     norm
+
 
 (** [nf ctx e] computes the normal form of expression [e]. *)
 let nf ctx e = norm ~weak:false ctx e
