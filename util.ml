fc4b775f82cf14747d2502c3a68a50a0442f3cb3
Francisco Ferreira <fferre8@cs.mcgill.ca>
2014-06-06 19:37:23 -0400

    adding some utilities

--- util.ml
+++ util.ml
@@ -2,3 +2,11 @@ let rec zip l1 l2 = match l1, l2 with
   | [], [] -> []
   | x::l1', y::l2' -> (x,y):: zip l1' l2'
   | _ -> Error.violation "zip called with lists of different length"
+
+let maybe r f = 
+  match r with Some a -> a 
+	     | None -> f ()
+
+let impossible = fun _ -> Error.violation "Impossible! This just did not happen."
+
+let this = function | Some x -> x | _ -> impossible ()
