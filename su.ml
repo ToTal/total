61f382d9cf6b35c9e5051ad4976a1f02a5a9ed6c
Francisco Ferreira <fferre8@cs.mcgill.ca>
2014-06-11 17:26:24 -0400

    fixed stoopeed bug

--- su.ml
+++ su.ml
@@ -46,11 +46,11 @@ let split_head_spine ctx e =
   (* 	      (Print.expr ctx e) *)
   (* 	      (Print.expr ctx h) *)
   (* 	      (Print.sequence ~sep:" ;" (fun e -> Print.expr ctx e) sp) ; *)
-  h, List.rev sp
+  h, sp
 	       
 
 let join_head_spine h sp =
-  List.fold_left (fun h sp -> Common.nowhere(App(h, sp))) h (List.rev sp)
+  List.fold_left (fun h sp -> Common.nowhere(App(h, sp))) h sp
 
 (* Returns wether e constains the constant d in its head *)
 let is_constr ctx d e = 
